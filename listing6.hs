2d61839c899eadb782283e5c5edf6e0013ef6506
foest <foest88@gmail.com>
2013-03-05 17:36:26 -0800

    fixed showVal for Bool

--- listing6.hs
+++ listing6.hs
@@ -32,7 +32,7 @@ showVal (String contents) = "\"" ++ contents ++ "\""
 showVal (Atom name) = name
 showVal (Number contents) = show contents
 showVal (Bool True) = "#t"
-showVal (Bool False) = "#t"
+showVal (Bool False) = "#f"
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
 showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
 
