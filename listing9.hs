1873eb52e431f100170b994abe3997107e3c8ca1
foest <foest88@gmail.com>
2013-03-14 13:08:02 -0700

    update showVal for new constructors

--- listing9.hs
+++ listing9.hs
@@ -99,6 +99,12 @@ showVal (Bool True) = "#t"
 showVal (Bool False) = "#f"
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
 showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+showVal (PrimitiveFunc _) = "<primitive>"
+showVal (Func {params = args, vararg = varargs, body = body, closure = env}) =
+  "(lambda (" ++ unwords (map show args) ++
+     (case varargs of
+        Nothing -> ""
+        Just arg -> " . " ++ arg) ++ ") ...)"
 
 unwordsList = unwords . map showVal
 
