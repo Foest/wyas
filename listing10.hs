47987c4d8262b119d4c5359415bbb47c93656a41
foest <foest88@gmail.com>
2013-03-16 13:34:04 -0700

    add read/write/apply wrappers

--- listing10.hs
+++ listing10.hs
@@ -524,6 +524,27 @@ ioPrimitives = [("apply", applyProc),
                 ("read-contents", readContents),
                 ("read-all", readAll)]
 
+applyProc [func, List args] = apply func args
+applyProc (func : args) = apply func args
+
+makePort mode [String filename] = liftM Port $ liftIO $ openFile filename mode
+
+closePort [Port port] = liftIO $ hClose port >> (return $ Bool True)
+closePort _ = return $ Bool False
+
+readProc [] = readProc [Port stdin]
+readProc [Port port] = (liftIO $ hGetLine port) >>= liftThrows . readExpr
+
+writeProc [obj] = writeProc [obj, Port stdout]
+writeProc [obj, Port port] = liftIO $ hPrint port obj >> (return $ Bool True)
+
+readContents [String filename] = liftM String $ liftIO $ readFile filename
+
+load filename = (liftIO $ readFile filename) >>= liftThrows . readExprList
+
+readAll [String filename] = liftM List $ load filename
+
+
 --------end evaluator--------
 
 
